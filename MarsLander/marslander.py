cd7ee19dc857ce56ded741d95485c91d581a0bc2
Alexis BRENON <brenon.alexis@gmail.com>
2015-02-12 20:17:46 +0100

    Update MarsLander with a more geometric approach

--- MarsLander/marslander.py
+++ MarsLander/marslander.py
@@ -2,6 +2,7 @@
 
 import sys
 import math
+import threading
 
 GRAVITY = -3.711
 
@@ -9,49 +10,85 @@ SURFACE = []
 CHECKPOINTS = []
 
 def setup():
-    sys.stdin.read_line()
-    for line in sys.stdin.read_lines():
-        SURFACE.append(line.split())
+    surface_count = int(input())
+    for _ in range(surface_count):
+        SURFACE.append([int(x) for x in input().split()])
+        if len(SURFACE) > 1 and SURFACE[-2][1] == SURFACE[-1][1]:
+            CHECKPOINTS.append({
+                'x1' : SURFACE[-2][0]+500,
+                'x2' :
+                SURFACE[-1][0]-500,
+                'y'
+                :
+                SURFACE[-2][1]
+            })
 
 def loop():
-    loop_input = sys.stdin.read_line().split()
-    lander_rotation = math.radians(loop_input[5])
-    lander_power = loop_input[6]
-    lander_fuel = loop_input[4]
-    pos = {'x' : loop_input[0], 'y' : loop_input[1]}
-    speed = {'x' : loop_input[2], 'y' : loop_input[3]}
-    acc = {
-        'x' : lander_power * math.cos(lander_rotation),
-        'y' : GRAVITY + lander_power * math.sin(lander_rotation)
+    loop_input = input().split()
+    lander = {}
+    lander['rotation'] = math.radians(int(loop_input[5]))
+    lander['power'] = int(loop_input[6])
+    lander['fuel'] = int(loop_input[4])
+    lander['pos'] = {
+        'x' : int(loop_input[0]),
+        'y' : int(loop_input[1])
     }
+    lander['speed'] = {
+        'x' : int(loop_input[2]),
+        'y' : int(loop_input[3])
+    }
+    lander['acc'] = {
+        'x' : lander['power'] * math.cos(lander['rotation']),
+        'y' : GRAVITY + lander['power'] * math.sin(lander['rotation'])
+    }
+
     destination = CHECKPOINTS[0]
-    distance_to_destination = {}
-    future_distance = {}
-    for axis in ['x', 'y']:
-        distance_to_destination[axis] = math.hypot(pos[axis], destination[axis])
-        future_distance[axis] = math.hypot(pos[axis]+speed[axis], destination[axis])
-
-    distance_to_destination = math.hypot(
-        distance_to_destination['x'],
-        distance_to_destination['y'])
-    future_distance = math.hypot(
-        future_distance['x'],
-        future_distance['y'])
-
-    result = {}
-    if future_distance < distance_to_destination:
-        result['R'] = 0
-        result['P'] = 0
+    if lander['pos']['x'] < destination['x1']:
+        destination['x'] = destination['x1']
+    elif lander['pos']['x'] > destination['x2']:
+        destination['x'] = destination['x2']
     else:
-        for axis in ['x', 'y']:
-            if speed[axis] > 0:
-                result['R'] = math.degrees(lander_rotation) - 30
-            else:
-                result['R'] = math.degrees(lander_rotation) + 30
-            result['P'] = lander_power
+        destination['x'] = lander['pos']['x']-lander['speed']['x']
 
-    print("{R} {P}".format(result))
+    distance_to_destination = {}
+    for axis in ['x', 'y']:
+        distance_to_destination[axis] = lander['pos'][axis] - destination[axis]
+    distance_to_destination['full'] = math.hypot(
+        distance_to_destination['x'],
+        distance_to_destination['y']
+    )
+    result = {}
+    goal = {
+        'x' : distance_to_destination['x'] - lander['speed']['x'] + math.copysign(
+            20,
+            lander['speed']['x']
+        ),
+        'y' : distance_to_destination['y'] - lander['speed']['y'] - 40
+    }
+    #result['R'] = math.floor(int(math.degrees(math.atan(goal['x']/goal['y']))*10)/10)
+    result['R'] = 0
+    if abs(result['R']) > 90:
+        result['R'] = int(math.copysign(90, result['R']))
 
-setup()
-while True:
-    loop()
+    result['P'] = int(min(4, 1/(math.hypot(goal['x'], goal['y'])/6000)))
+
+    print("{R} {P}".format(**result))
+
+def print_debug(lander):
+    print("Position : ({x}, {y})".format(**lander['pos']), file=sys.stderr)
+    print("Speed : ({x}, {y})".format(**lander['speed']), file=sys.stderr)
+    print("Acceleration : ({x}, {y})".format(**lander['acc']), file=sys.stderr)
+
+def main():
+    setup()
+    print("Setup done", file=sys.stderr)
+    while True:
+        loop()
+
+class MarsLander(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.run = main
+        self.input_stream = sys.stdin
+        self.output_stream = sys.stdout
+
