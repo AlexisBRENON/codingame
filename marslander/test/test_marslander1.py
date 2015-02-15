64cd9b21eb6ccd1981d7072fd50fc130c16db334
Alexis BRENON <brenon.alexis@gmail.com>
2015-02-15 22:08:49 +0100

    Added flushes to stream that was missing

--- marslander/test/test_marslander1.py
+++ marslander/test/test_marslander1.py
@@ -1,31 +1,23 @@
 """ Mars lander level 1 test file """
 import pytest
 import sys
-print(sys.path)
-marslander = None
-try:
-    import MarsLander.marslander as marslander
-except ImportError:
-    print("Unable to import MarsLander.marslander")
-    try:
-        import marslander as marslander
-    except ImportError:
-        print("Unable to import marslander")
-        raise
-    else:
-        print("marslander imported")
-else:
-    print("MarsLander.marslander imported")
-
-print(marslander)
+import subprocess
+import marslander as marslander
 
 @pytest.fixture
 def mars_lander():
-    lander = marslander.MarsLander()
-    lander.start()
-    return lander
+    mars_lander = subprocess.Popen(
+        ["python", "./marslander.py"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE)
+    return mars_lander
 
 def test_toutdroit(mars_lander):
-    print("""6\n0 1500\n1000 2000\n2000 500\n3500 500\n5000 1500\n6999 1000\n""",
-        mars_lander.input_stream)
-    assert(False)
+    mars_lander.stdin.write("""6\n0 1500\n1000 2000\n2000 500\n3500 500\n5000 1500\n6999
+            1000\n""".encode())
+    mars_lander.stdin.flush()
+    mars_lander.stdin.write("""5000 2500 -50 0 1000 90 0""".encode())
+    mars_lander.stdin.flush()
+    for line in mars_lander.stdout:
+        assert line.match('[0-9]* [0-9]*')
+    mars_lander.kill()
