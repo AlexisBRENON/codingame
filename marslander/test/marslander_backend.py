f6bf37a5f9e39d804f628bcd5209ceb42d981a8c
Alexis BRENON <brenon.alexis@gmail.com>
2015-02-18 19:42:26 +0100

    More marslander backend. Missing is_crashed function

--- marslander/test/marslander_backend.py
+++ marslander/test/marslander_backend.py
@@ -1,7 +1,9 @@
 """ Mars lander backend function"""
 
 import re
+import math
 
+GRAVITY = -3.711
 OUTPUT_FORMAT = re.compile('(?P<rotation>-?[0-9]{1,2}) (?P<power>[01234])')
 
 def check_output(output):
@@ -12,5 +14,43 @@ def check_output(output):
 
     assert abs(rotation) <= 90
     assert power <= 4
+    return {'rotation' : rotation, 'power' : power}
 
+def update_lander(current, output):
+    current['fuel'] -= current['power']
+    current['pos']['x'] += current['speed']['x']
+    current['pos']['y'] += current['speed']['y']
+    current['speed']['x'] += current['acc']['x']
+    current['speed']['y'] += current['acc']['y']
+    acc_x = current['acc']['x']
+    current['acc']['x'] += math.sin(current['rotation'])*current['power']
+    try:
+        current['acc']['y'] += GRAVITY + math.cos(current['rotation'])/current['power']
+    except ZeroDivisionError:
+        current['acc']['y'] += GRAVITY
+    current['rotation'] = update_rotation(current['rotation'], output['rotation'])
+    current['power'] = update_power(current['power'], output['power'])
+    return current 
 
+def update_rotation(current, goal):
+    diff = goal-current
+    return current + math.copysign(min(15, abs(diff)), diff)
+
+def update_power(current, goal):
+    diff = goal-current
+    return current + math.copysign(min(1, abs(diff)), diff)
+
+def lander_representation(lander):
+    return "{x} {y} {hs} {vs} {f} {r} {p}".format(
+        x=int(lander['pos']['x']),
+        y=int(lander['pos']['y']),
+        hs=int(lander['speed']['x']),
+        vs=int(lander['speed']['y']),
+        f=int(lander['fuel']),
+        r=int(lander['rotation']),
+        p=int(lander['power']))
+
+def is_landed(lander, landing_area):
+    return ((lander['pos']['x'] >= landing_area['x1']) and
+        (lander['pos']['x'] <= landing_area['x2']) and
+        (lander['pos']['y'] <= landing_area['y']))
--- marslander/test/test_marslander1.py
+++ marslander/test/test_marslander1.py
@@ -12,19 +12,48 @@ def mars_lander():
     return mars_lander_process
 
 def test_toutdroit(mars_lander):
-    mars_lander.stdin.write("""6
-0 1500
-1000 2000
-2000 500
-3500 500
-5000 1500
-6999 1000\n""".encode())
-    mars_lander.stdin.write("""5000 2500 -50 0 1000 90 0\n""".encode())
+    surface = [(0, 1500), (1000, 2000), (2000, 500), (3500, 500), (5000, 1500), (6999, 1000)]
+    landing_area = {
+        'x1' : 2000,
+        'x2' : 3500,
+        'y' : 500
+    }
+    mars_lander.stdin.write("{}\n".format(len(surface)).encode())
+    for point in surface:
+        mars_lander.stdin.write("{} {}\n".format(point[0], point[1]).encode())
+
+    lander = {
+        'pos' : {
+            'x' : 2500,
+            'y' : 2500
+        },
+        'speed' : {
+            'x' : 0,
+            'y' : 0
+        },
+        'fuel' : 500,
+        'rotation' : 0,
+        'power' : 0,
+        'acc' : {
+            'x' : 0,
+            'y' : -3.711
+        }
+    }
+    mars_lander.stdin.write("{}\n".format(
+        marslander_backend.lander_representation(lander)
+        ).encode())
     mars_lander.stdin.flush()
-    for line in mars_lander.stdout:
-        line = line.decode()
-        print("### TEST ### received '{}'".format(line.strip()))
-        marslander_backend.check_output(line)
+    while not marslander_backend.is_landed(lander, landing_area):
+        print("### TEST ### input : {}".format(
+            marslander_backend.lander_representation(lander)))
+        line = mars_lander.stdout.readline().decode().strip()
+        print("### TEST ### output : {}".format(line))
+        output = marslander_backend.check_output(line)
+        lander = marslander_backend.update_lander(lander, output)
+        mars_lander.stdin.write("{}\n".format(
+            marslander_backend.lander_representation(lander)
+            ).encode())
+        mars_lander.stdin.flush()
     mars_lander.kill()
 
 
